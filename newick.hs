13c52a361158da25d6c04ea0f0ecaaa9e8dec8df
ballesterus <chuuuuy@gmail.com>
2023-06-19 15:50:52 -0500

    blah

--- newick.hs
+++ newick.hs
@@ -1,8 +1,10 @@
 import qualified Data.List as List
 
--- test tree-  let s = (U_glomosus|016215c55186_g4_i7:0.00055,U_glomosus|016210c55186_g4_i2:0.00288,((N_sinensis|002764c32854_g1_i1:0.04246,((((N_clavipes|005148c24804_g1_i1:0.23844,((N_arabesca|008677c46582_g1_i2:0.06151,(N_arabesca|008678c46582_g1_i3:0.00055,N_arabesca|008676c46582_g1_i1:0.00055)0.858:0.00055)0.958:0.03397,(G_hasselti|021872c46388_g1_i1:0.00587,(G_arcuata|000220c11540_g1_i1:0.00055,G_arcuata|000221c11540_g1_i2:0.00055)0.978:0.00885)0.998:0.06477)0.786:0.02409)0.584:0.03585,((E_leonina|022467c55244_g1_i1:0.04891,(M_guttata|030435c44002_g2_i3:0.00055,(M_guttata|030437c44002_g2_i5:0.00987,M_guttata|030436c44002_g2_i4:0.00055)0.983:0.01318)1.000:0.18070)0.742:0.02883,(L_venusta|028454c57950_g3_i1:0.05356,(T_perreira|012195c15901_g1_i1:0.00055,T_polychromata|011228c37131_g1_i1:0.02380)0.997:0.09783)0.961:0.04252)0.955:0.02889)0.868:0.01330,(F_communis|030772c61053_g3_i1:0.01551,F_communis|030773c61053_g3_i2:0.00055)1.000:0.09954)0.892:0.03202,((H_pococki|005985c27614_g1_i1:0.00053,H_pococki|006005c27653_g1_i1:0.00055)0.998:0.32956,(D_longipes|008825c32992_g1_i1:0.00055,D_longipes|009202c33745_g1_i1:0.00054)0.973:0.25748)0.273:0.09471)0.985:0.07772)0.999:0.04988,(U_glomosus|016209c55186_g4_i1:0.00055,U_glomosus|016217c55186_g4_i9:0.00055)0.996:0.00054)1.000:0.00375);
+-- test tree-  let s = "(U_glomosus|016215c55186_g4_i7:0.00055,U_glomosus|016210c55186_g4_i2:0.00288,((N_sinensis|002764c32854_g1_i1:0.04246,((((N_clavipes|005148c24804_g1_i1:0.23844,((N_arabesca|008677c46582_g1_i2:0.06151,(N_arabesca|008678c46582_g1_i3:0.00055,N_arabesca|008676c46582_g1_i1:0.00055)0.858:0.00055)0.958:0.03397,(G_hasselti|021872c46388_g1_i1:0.00587,(G_arcuata|000220c11540_g1_i1:0.00055,G_arcuata|000221c11540_g1_i2:0.00055)0.978:0.00885)0.998:0.06477)0.786:0.02409)0.584:0.03585,((E_leonina|022467c55244_g1_i1:0.04891,(M_guttata|030435c44002_g2_i3:0.00055,(M_guttata|030437c44002_g2_i5:0.00987,M_guttata|030436c44002_g2_i4:0.00055)0.983:0.01318)1.000:0.18070)0.742:0.02883,(L_venusta|028454c57950_g3_i1:0.05356,(T_perreira|012195c15901_g1_i1:0.00055,T_polychromata|011228c37131_g1_i1:0.02380)0.997:0.09783)0.961:0.04252)0.955:0.02889)0.868:0.01330,(F_communis|030772c61053_g3_i1:0.01551,F_communis|030773c61053_g3_i2:0.00055)1.000:0.09954)0.892:0.03202,((H_pococki|005985c27614_g1_i1:0.00053,H_pococki|006005c27653_g1_i1:0.00055)0.998:0.32956,(D_longipes|008825c32992_g1_i1:0.00055,D_longipes|009202c33745_g1_i1:0.00054)0.973:0.25748)0.273:0.09471)0.985:0.07772)0.999:0.04988,(U_glomosus|016209c55186_g4_i1:0.00055,U_glomosus|016217c55186_g4_i9:0.00055)0.996:0.00054)1.000:0.00375);"
 
-splitOn ::  [Char] ->[Char] -> [[Char]]
+
+
+splitOn ::  [Char] ->[Char] -> [[Char]] --- I may need to get rid off this fuction (is not used)
 splitOn x xs = case dropWhile (`elem` x) xs of
                  "" -> []
                  xs'  -> a : splitOn x xs''
@@ -10,15 +12,18 @@ splitOn x xs = case dropWhile (`elem` x) xs of
                            break (`elem` x) xs'
 
 
-parent :: [Char] -> ([Int], [Int]) -- Takes a newick string and returns a tuple of two list with the indexes of opening and closing parentheses.
-parent xs = (open,close)
-  where
-    open =  '(' `List.elemIndices` xs
-    close = ')' `List.elemIndices` xs 
+openParent :: [Char] -> [Int] -- Takes a newick string and returns a tuple of two list with the indexes of opening and closing parentheses.
+openParent xs = '(' `List.elemIndices` xs
 
-pairParent :: ([Int],[Int]) ->[(Int,Int)] -- This function matches paiired parentheses  
-pairParent ([],[])  = []
-pairParent (o,(e:c)) = (a,e): pairParent (b,c)
+
+closeParent :: [Char] ->  [Int] -- Takes a newick string and returns a list with the indexes of closing parentheses.
+closeParent xs =  ')' `List.elemIndices` xs 
+
+
+pairParent :: [Int] -> [Int] ->[(Int, Int)] -- This function matches paiired parentheses
+pairParent [] _  = []
+pairParent _ [] = []
+pairParent o (e:c) = (a,e): pairParent b c
   where a = lastofLess o e
         b = List.delete a o
 
@@ -64,6 +69,13 @@ issameSplit x y
   | x == antisplit y = True
   | otherwise = False
 
+
+-- assymDiff :: [String] -> [String] -> [Bool] -- where the list of input strings are list of bipartitions
+-- assymDiff [] (ys)  =  []
+-- assymDiff (x:xs) ys = foldl (\x => if issameSplit x then ) False ys: assymDiff xs ys
+  
+
+
 split2Taxlist :: String -> [String] -> [String] --Takes a split string, a list of taxa and returns a collection of taxa represented in the split
 split2Taxlist [] _ = []
 split2Taxlist _ [] = []
@@ -75,7 +87,9 @@ split2Taxlist (s:plit) (t:axa)
 splitDecomposition :: String -> [String] --A tree is a collection of splits
 splitDecomposition newick = [split taxa (getleaves (withinBound a newick)) | a <- fromParentheses]
   where taxa = getleaves newick
-        fromParentheses = pairParent $ parent newick
+        open = openParent newick
+        close = closeParent newick
+        fromParentheses = pairParent open close
 
 
 sameTaxa :: String -> String -> Bool
@@ -85,3 +99,32 @@ sameTaxa x y
   | otherwise = False
 
                          
+
+getSupports :: String -> [Float]
+getSupports tree 
+  | sub == "" = []
+  | support == ";" = 100.0 : getSupports (tail sub) 
+  | support == "" = 0 :  getSupports (tail sub) 
+  | otherwise = read support : getSupports (tail sub) 
+  where
+    sub =  dropWhile (/=')') tree
+    support =  tail $ takeWhile (/= ':') sub
+
+
+---math and stats
+
+mean :: [Float] -> Float
+mean [] = 0
+mean xs = ss /fromIntegral  n
+  where ss = sum xs
+        n = length xs
+
+
+variance :: [Float] -> Float
+variance xs = ssdiff / fromIntegral n
+  where ssdiff = sum [(x - m)^2 | x <- xs]
+        m = mean xs
+        n = (length xs) - 1
+
+sd :: [Float] -> Float
+sd xs = sqrt $ variance xs
